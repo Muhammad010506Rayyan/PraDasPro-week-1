a875ac615d42c38d749ea1fb2367442071dcbda8
Muhammad_Rayyan <hairay261@gmail.com>
2025-10-13 15:54:56 +0700

    ExerciseBeforeMiddleTest

--- PRADASPRO-JOBSHEET6/caseStudy.java
+++ PRADASPRO-JOBSHEET6/caseStudy.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class studyCase {
+public class caseStudy {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
      String message;
