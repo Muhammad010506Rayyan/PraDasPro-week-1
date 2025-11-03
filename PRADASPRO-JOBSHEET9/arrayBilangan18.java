dbf9ab3a9d29b856e38cf85a4c61e58bf8e827d1
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-03 21:59:53 +0700

    array using for loop

--- PRADASPRO-JOBSHEET9/arrayBilangan18.java
+++ PRADASPRO-JOBSHEET9/arrayBilangan18.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class arrayBilangan18 {
     public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    
     int [] bil=new int[4];
     //Isi masing-masing elemen array bil tadi dengan angka 5, 13, -7, 17.
     bil [0]=5 ;
@@ -10,8 +13,14 @@ public class arrayBilangan18 {
     System.out.println(bil[1]);
     System.out.println(bil[2]);
     System.out.println(bil[3]);
+/*Jika isi masing-masing elemen array bil diubah dengan angka 5.0, 12867, 7.5, 2000000.
+Apa yang terjadi? Mengapa bisa demikian?
+Answer: it will be error for the decimal number because we use int data type not double or float data type*/
 
-
+//make the for loop to switch the line 9
+for(int i=0;i<bil.length;i++){
+    System.out.println("ini dia bilangannya: "+ bil[i]);
+}
 
     }
     
