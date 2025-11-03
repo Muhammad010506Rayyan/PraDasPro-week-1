0473a32ff0b7034dbf37a91b9f272f138170b17b
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-03 22:56:43 +0700

    input array

---
+++ PRADASPRO-JOBSHEET9/arrayNilai.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class arrayNilai {
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    // make the for loop to input the value
+int [] nilai= new int[5];
+for(int i=0;i<nilai.length;i++){
+    System.out.print("input your value: ");
+    nilai[i]=sc.nextInt();
+    //make the output using loop for
+}for(int i=0;i<nilai.length;i++){
+    System.out.println("your value is: "+ nilai[i]);
+}    
+}
+}
