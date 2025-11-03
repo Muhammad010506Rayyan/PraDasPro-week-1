14beb3c461b8773e15808811f09324aea6d23cdb
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-03 23:17:33 +0700

    array if

--- PRADASPRO-JOBSHEET9/arrayNilai.java
+++ PRADASPRO-JOBSHEET9/arrayNilai.java
@@ -5,11 +5,13 @@ public static void main(String[] args) {
     // make the for loop to input the value
 int [] nilai= new int[5];
 for(int i=0;i<nilai.length;i++){
-    System.out.print("input your value: ");
+    System.out.println("input your value: ");
     nilai[i]=sc.nextInt();
     //make the output using loop for
-}for(int i=0;i<nilai.length;i++){
-    System.out.println("your value is: "+ nilai[i]);
-}    
+}for (int i=0;i<nilai.length;i++){
+    if(nilai[i] > 70){
+        System.out.println("your grade is: "+nilai[i]+", congrats, you pass!");
+    }else{System.out.println("you got remidial");}
+}
 }
 }
