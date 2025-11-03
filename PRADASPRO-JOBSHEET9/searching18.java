485a8b502f161bef58e98d917355a5dbdfc478ee
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-04 00:10:44 +0700

    searching

--- PRADASPRO-JOBSHEET9/searching18.java
+++ PRADASPRO-JOBSHEET9/searching18.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class searching18 {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
     int[] nilai={87,88,89,90,91};
     int hasil=0;
-    int key=90;
+    int key=sc.nextInt();
 //make the if program
         for(int i=0;i<nilai.length;i++){
             if(key==nilai[i]){
                 hasil=i;
-                System.out.println("the key "+key+ " was stored in index "+ hasil+ "or in element"+(hasil+1));
-            }
-        }
+                System.out.println("the key "+key+ " was stored in index "+ hasil+ " or in element "+(hasil+1));
+            }  
+        } 
     }
 }
