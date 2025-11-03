5e45b91f7bb4c9e57e4c9c77083325bddfd616e6
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-03 21:01:18 +0700

    array 1

---
+++ PRADASPRO-JOBSHEET9/arrayBilangan18.java
@@ -0,0 +1,12 @@
+public class arrayBilangan18 {
+    public static void main(String[] args) {
+    int [] bil=new int[4];
+    bil [0]=1;
+    bil [1]=2;
+    bil [2]=3;
+    bil [3]=4;
+
+
+    }
+    
+}
