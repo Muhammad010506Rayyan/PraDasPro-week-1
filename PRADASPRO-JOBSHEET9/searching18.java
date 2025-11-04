5f3bb7028c08972621b44c103c4c4b6126ede4fa
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-04 07:51:03 +0700

    array when you dont find the key

--- PRADASPRO-JOBSHEET9/searching18.java
+++ PRADASPRO-JOBSHEET9/searching18.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 public class searching18 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-    int[] nilai={87,88,89,90,91};
     int hasil=0;
+        //initialize the array value
+    int[] nilai={87,88,89,90,91};
+        //input the value that you want to search
     System.out.println("input the value:");
     int key=sc.nextInt();
-//make the if program
+        //make the if program
         for(int i=0;i<nilai.length;i++){
             if(key==nilai[i]){
-                hasil=i;
-                System.out.println("the key "+key+ " was stored in index "+ hasil+ " or in element "+(hasil+1));
-            }
+            hasil=i;
+            break;
         }
+        }
+        if (hasil != 0) {
+            System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
+
     }
 }   
    
