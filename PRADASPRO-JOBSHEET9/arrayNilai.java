e1eb87dcd3fb18ea90dc45599a3e07023d16245d
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-04 09:08:29 +0700

    array

--- PRADASPRO-JOBSHEET9/arrayNilai.java
+++ PRADASPRO-JOBSHEET9/arrayNilai.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 public class arrayNilai {
 public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
+     int sum= 0;
     // make the for loop to input the value
 int [] nilai= new int[5];
 for(int i=0;i<nilai.length;i++){
     System.out.println("input your value: ");
     nilai[i]=sc.nextInt();
+    sum += nilai[i];
     //make the output using loop for
 }for (int i=0;i<nilai.length;i++){
     if(nilai[i] > 70){
         System.out.println("your grade is: "+nilai[i]+", congrats, you pass!");
-    }else{System.out.println("you got remidial");}
-}
+    }else{System.out.println("you got remidial");break;}
+} System.out.println("average is: "+( sum / nilai.length));
+  
 }
 }
--- PRADASPRO-JOBSHEET9/searching18.java
+++ PRADASPRO-JOBSHEET9/searching18.java
@@ -23,5 +23,12 @@ public class searching18 {
 
     }
 }   
+/*  Jelaskan maksud dari statement break; pada baris ke-10 kode program percobaan 4 di
+atas.
+answer: to finish the loop after the loop is end, so the program below will not continue the loop
+2. Modifikasi kode program pada percobaan 4 di atas sehingga program dapat menerima
+input berupa banyaknya elemen array nilai, isi array, dan sebuah nilai (key) yang ingin
+dicari. Lalu cetak ke layar indeks posisi elemen dari nilai (key) yang dicari. Contoh hasil
+program: */
    
 
