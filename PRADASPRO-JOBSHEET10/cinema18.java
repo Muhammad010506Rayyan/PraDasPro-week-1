c7e3d1a8ea493cd78f531c16e3f4e9dac388fe8b
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-11 07:53:26 +0700

    experiment 1 cinema

---
+++ PRADASPRO-JOBSHEET10/cinema18.java
@@ -0,0 +1,23 @@
+public class cinema18 {
+public static void main(String[] args){
+String [][]audience=new String[4][2];
+audience[0][0]="Amir";
+audience[0][1]="B";
+audience[1][0]="C";
+audience[1][1]="D";
+audience[2][0]="E";
+audience[2][1]="F";
+audience[3][0]="G";
+audience[3][1]="H";
+
+
+System.out.printf(" %s\tand %s \n",audience[0][0],audience[0][1]);
+System.out.printf(" %s\tand %s \n",audience[1][0],audience[1][1]);
+System.out.printf(" %s\tand %s \n",audience[2][0],audience[2][1]);
+System.out.printf(" %s\tand %s \n",audience[3][0],audience[3][1]);
+
+
+
+    }
+    
+}
