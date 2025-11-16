04e7da8829a4aee19174df4105a22f7068930ef8
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-16 11:22:19 +0700

    1st question 2.3experiment

--- PRADASPRO-JOBSHEET 10/number18.java
+++ PRADASPRO-JOBSHEET 10/number18.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class number18 {
 public static void main(String[]args){
     //DECLARE ARRAY 2 DIMENTION
@@ -5,7 +6,11 @@ int[][]myNumber=new int[3][];
 myNumber[0]=new int[5];
 myNumber[1]=new int[3];
 myNumber[2]=new int[1];
-    //
+    //1st question
+for(int i=0;i<myNumber.length;i++){
+    System.out.println(Arrays.toString(myNumber[i]));
+}// the function is to show the element or the value for each column in each row.
+
 
 
     }
