3344d41f9a26b67bece9a9892d5b37a175cc5152
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-16 11:16:46 +0700

    experiment2.3

---
+++ PRADASPRO-JOBSHEET 10/number18.java
@@ -0,0 +1,12 @@
+public class number18 {
+public static void main(String[]args){
+    //DECLARE ARRAY 2 DIMENTION
+int[][]myNumber=new int[3][];
+myNumber[0]=new int[5];
+myNumber[1]=new int[3];
+myNumber[2]=new int[1];
+    //
+
+
+    }
+}
