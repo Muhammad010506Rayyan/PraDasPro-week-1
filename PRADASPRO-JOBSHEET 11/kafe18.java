1b4b317778d8c80908b349b4aa5335fd0434bc0b
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-26 07:30:13 +0700

    kafe18

---
+++ PRADASPRO-JOBSHEET 11/kafe18.java
@@ -0,0 +1,14 @@
+import java.util.*;//import all util library
+
+public class kafe18{
+public static void main(String[] args) {
+    menu();
+}
+
+public static void menu(){
+System.out.println("==CAFE MENU==");
+System.out.println("KOPI");
+System.out.println("SUSU");
+System.out.println("teh");
+    }
+}
